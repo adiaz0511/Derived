7de57065516a138b833e0e42ed5a990e7b196cd9
Arturo Diaz <arturo@devlabs.mx>
2026-07-31 15:11:49 -0600

    Initial Commit

---
+++ XcodeClean/XcodeCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCleanApp.swift
+//  XcodeClean
+//
+//  Created by Arturo Diaz on 7/31/26.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
